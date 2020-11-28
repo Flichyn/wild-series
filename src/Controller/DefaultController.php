9af564d319adde5a0e50dcc7a5bf7c23164689d0
Flichyn <nicolasflichy@gmail.com>
2020-11-28 16:23:02 +0100

    Made Symfony quest 03

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    /**
+     * @Route ("/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
